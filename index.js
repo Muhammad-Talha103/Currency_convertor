fd3ff648bba73b460fd4c04dab6605a56d0e8128
Muhammad Talha <talhagp908@gmail.com>
2024-04-06 03:33:34 -0230

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+import inquirer from "inquirer";
+const currency = {
+    USD: 1,
+    SAR: 3.75,
+    PKR: 277.70,
+    IRR: 42000,
+    EUR: 0.92,
+    INR: 83.29,
+};
+let retry = true;
+while (retry) {
+    let input = await inquirer.prompt([
+        {
+            type: "list",
+            name: "from",
+            message: "Select Currency From :",
+            choices: ["USD", "SAR", "PKR", "IRR", "EUR", "INR"]
+        },
+        {
+            type: "list",
+            name: "to",
+            message: "Select Currency To :",
+            choices: ["USD", "SAR", "PKR", "IRR", "EUR", "INR"]
+        },
+        {
+            type: "input",
+            name: "ammunt",
+            message: "Enter Your Amount"
+        }
+    ]);
+    let ammuntFrom = currency[input.from];
+    let ammuntTo = currency[input.to];
+    let ammunt = input.ammunt;
+    let amountBase = ammunt / ammuntFrom;
+    let ammuntConvert = amountBase * ammuntTo;
+    console.log(ammuntConvert.toFixed(2));
+    const again = await inquirer.prompt({
+        type: "confirm",
+        name: "again",
+        message: "Did You Want To Try Again ?",
+        default: false
+    });
+    retry = again.again;
+}
---
+++ index.ts
@@ -0,0 +1,60 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+const currency:any={
+    USD:1,
+    SAR:3.75,
+    PKR:277.70,
+    IRR:42000,
+    EUR:0.92,
+    INR:83.29,
+}
+let retry:boolean=true
+
+while(retry){
+let input=await inquirer.prompt(
+    [
+        {
+            type:"list",
+            name:"from",
+            message:"Select Currency From :",
+            choices:["USD","SAR","PKR","IRR","EUR","INR"]
+        },
+        {
+            type:"list",
+            name:"to",
+            message:"Select Currency To :",
+            choices:["USD","SAR","PKR","IRR","EUR","INR"]
+        },
+        {
+            type:"input",
+            name:"ammunt",
+            message:"Enter Your Amount"
+        }
+    ]
+)
+let ammuntFrom=currency[input.from];
+
+let ammuntTo=currency[input.to];
+
+let ammunt=input.ammunt;
+
+let amountBase=ammunt/ammuntFrom;
+
+let ammuntConvert=amountBase*ammuntTo
+
+
+console.log(ammuntConvert.toFixed(2));
+
+const again=await inquirer.prompt(
+    {
+        type:"confirm",
+        name:"again",
+        message:"Did You Want To Convert More ?",
+        default : false
+    }
+)
+retry=again.again
+}
